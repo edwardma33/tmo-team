57e340a8344c22e116b98fbe5643440f5f3827e4
Maddox Edwards <edwardma33@maddogs-mac.lan>
2023-04-04 23:44:40 -0400

    TMO-ON-HOUK V-1.1 Fixed roles for dark mode

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -9,7 +9,7 @@ export default function Profile({person}) {
   return (
     <main className=' text-center p-5 rounded-3xl shadow-2xl dark:shadow-md dark:shadow-magenta mb-10'>
       <h2 className=' text-5xl py-2 text-magenta'>{team[person].name}</h2>
-      <h3 className=' text-2xl py-2'>{team[person].role}</h3>
+      <h3 className=' text-2xl py-2 dark:text-white'>{team[person].role}</h3>
       <div className='relative mx-auto bg-gradient-to-b from-magenta to-white rounded-full w-40 h-40 overflow-hidden shadow-2xl'>
         <Image src={require('../public/' + team[person].name.toLowerCase() + '.png')} fill alt='Employee Memoji'/>
       </div>
